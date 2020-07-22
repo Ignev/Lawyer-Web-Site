0c1de421019f4fc7327befd10b0439108892e7f0
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-07-23 01:22:01 +0300

    Add layout and adaptive

--- README.md
+++
@@ -1,1 +0,0 @@
-# Lawyer-Web-Site
---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+// Imports
+
+import form from "./form";
+import popup from "./popup";
+import slider from "./slider";
+import maps from "./maps";
+
+// Call functions
+
+window.addEventListener('DOMContentLoaded', () => {
+    form();
+    popup();
+    slider();
+    maps();
+});
